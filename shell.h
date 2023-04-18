956f115330071022445551aa6bac0edb2dd11e29
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-04-18 11:43:41 +0200

    added header file& string function file

---
+++ shell.h
@@ -0,0 +1,16 @@
+
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *_strcat(char *, char *);
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+
+
+#endif
